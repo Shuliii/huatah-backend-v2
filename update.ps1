fe8eb721bd63b63f67949b00371a0b033a340999
Rico <v_fharsono@tencent.com>
2024-08-28 14:41:52 +0800

    edit

---
+++ update.ps1
@@ -0,0 +1,30 @@
+# Build the Docker image
+try {
+    docker build -t ricoharsono/huatah-backend:latest . | Out-String
+} catch {
+    Write-Host "Docker build failed"
+    exit 1
+}
+
+# Push the Docker image to Docker Hub
+try {
+    docker push ricoharsono/huatah-backend:latest | Out-String
+} catch {
+    Write-Host "Docker push failed"
+    exit 1
+}
+
+# Check Kubernetes context
+$checkContext = kubectl config current-context | Select-String -Pattern "do-sgp1-k8s"
+
+if (-not $checkContext) {
+    Write-Host "Current context is not correct. Exiting."
+    exit 1
+} else {
+    try {
+        kubectl rollout restart deployment huatah-backend-deployment | Out-String
+    } catch {
+        Write-Host "Kubernetes rollout restart failed"
+        exit 1
+    }
+}
