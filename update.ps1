0ffb6c432229bf28bdd81c46675b6f64aa36409f
barkeleyrico <barkeley.rico@gmail.com>
2024-10-09 22:52:00 +0800

    edit

--- update.ps1
+++ update.ps1
@@ -1,4 +1,9 @@
 #Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process
+
+param(
+    [string]$Message
+)
+
 # Build the Docker image
 try {
     docker build -t ricoharsono/huatah-backend:latest . | Out-String
@@ -28,4 +33,21 @@ if (-not $checkContext) {
         Write-Host "Kubernetes rollout restart failed"
         exit 1
     }
+}
+
+#Send text message
+# Define your bot token and chat IDs
+$botToken = "902467435:AAGGJ695Sy_EWiFyJszxTkwTJqbxPwiNsG4"
+$chatIds = @("6454159736", "362851348")
+
+# Send the message to each chat ID
+foreach ($chatId in $chatIds) {
+    $url = "https://api.telegram.org/bot$botToken/sendMessage"
+    $body = @{
+        chat_id = $chatId
+        text    = $Message
+    }
+
+    # Send the POST request
+    Invoke-RestMethod -Uri $url -Method Post -Body $body
 }
